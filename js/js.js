709060cdead9aae316eec86c5857290119f1f84f
Yy2z <1615978027@qq.com>
2022-05-09 16:06:44 +0800

    gx

---
+++ js/js.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hxsd on 2016/9/21.
+ */
+$(document).ready(function(){
+    $('header').show();
+    $('#move').click(function(){
+        $('header').slideUp(1500);
+        $('#grjl').fadeIn(1500);
+    })
+});/**
+ * Created by 666 on 2016/12/11.
+ */
